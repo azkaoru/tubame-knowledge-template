2bb4d9d2eafa5d20e30adb93b4e370d102271949
watanabe kaoru <superkaoru@gmail.com>
2013-10-09 10:29:31 +0900

    template knowledge first commit

---
+++ userknowledge/programlisting/java/Foo.java
@@ -0,0 +1,20 @@
+
+package hoge;
+
+public class Foo {
+	private String hoge;
+
+	public Foo() {
+		super();
+	}
+
+	public String getHoge() {
+		return hoge;
+	}
+
+	public void setHoge(String hoge) {
+		this.hoge = hoge;
+	}
+	
+
+}
